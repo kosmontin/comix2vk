da2065764c1fae2c1f3434254777f1bf53e052f0
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-15 14:14:32 +0300

    post comix to wall community

--- main.py
+++ main.py
@@ -2,20 +2,43 @@ import os
 import requests
 from urllib.parse import urlparse
 from dotenv import load_dotenv
-from pprint import pprint
+
+VK_METHODS_URL = 'https://api.vk.com/method/'
 
 
-def upload_file_to_server(pathfile, upload_url):
-    url = upload_url
+def upload_file_to_server(upload_url, pathfile, comment=None):
     with open(pathfile, 'rb') as file:
-        files = {'photo': file}
-        response = requests.post(url, files=files)
+        response = requests.post(upload_url, files={'photo': file})
         response.raise_for_status()
+        server_answer = response.json()
+
+    params = {
+        'access_token': os.getenv('VK_ACCESS_TOKEN'),
+        'group_id': os.getenv('VK_GROUP_ID'),
+        'server': server_answer['server'],
+        'photo': server_answer['photo'],
+        'hash': server_answer['hash'],
+        'v': os.getenv('VK_API_VER')
+    }
+    url = VK_METHODS_URL + 'photos.saveWallPhoto'
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    url = VK_METHODS_URL + 'wall.post'
+    params = {
+        'access_token': os.getenv('VK_ACCESS_TOKEN'),
+        'attachments': f'photo{response.json()["response"][0]["owner_id"]}_{response.json()["response"][0]["id"]}',
+        'owner_id': -int(os.getenv('VK_GROUP_ID')),
+        'from_group': 1,
+        'message': comment,
+        'v': os.getenv('VK_API_VER')
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
     return response.json()
 
 
-def get_uploadserver_info():
-    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+def get_uploadserver_url():
+    url = VK_METHODS_URL + 'photos.getWallUploadServer'
     params = {
         'access_token': os.getenv('VK_ACCESS_TOKEN'),
         'group_id': os.getenv('VK_GROUP_ID'),
@@ -39,21 +62,26 @@ def get_vk_groups():
 
 
 def get_comix():
-    url = 'https://xkcd.com/619/info.0.json'
+    url = 'https://xkcd.com/630/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
     comix_content = response.json()
-    print(comix_content['alt'])
-    download_comix_img(comix_content['img'])
+    return {
+        'img_path': download_comix_img(comix_content['img']),
+        'comment': comix_content['alt']
+    }
 
 
 def download_comix_img(img_url):
+    path = 'images/' + os.path.basename(urlparse(img_url).path)
     response = requests.get(img_url)
     response.raise_for_status()
-    with open(os.path.basename(urlparse(img_url).path), 'wb') as file:
+    with open(path, 'wb') as file:
         file.write(response.content)
+    return path
 
 
 if __name__ == '__main__':
     load_dotenv()
-    pprint(upload_file_to_server('./supported_features.png', get_uploadserver_info()), sort_dicts=False)
+    comix = get_comix()
+    upload_file_to_server(get_uploadserver_url(), comix['img_path'], comix['comment'])
