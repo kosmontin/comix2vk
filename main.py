d60ecee555ab21721e15ac643e371f7be4c59fdd
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-18 16:13:56 +0300

    add check to errors in server response

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def upload_file_to_server(upload_url, filename):
     with open(filename, 'rb') as file:
         response = requests.post(upload_url, files={'photo': file})
         response.raise_for_status()
-    return response.json()
+    answer = response.json()
+    check_response(answer)
+    return answer
 
 
 def save_file_to_server(api_key, group_id, api_ver, server_answer):
@@ -27,7 +29,9 @@ def save_file_to_server(api_key, group_id, api_ver, server_answer):
     url = f'{VK_METHODS_URL}photos.saveWallPhoto'
     response = requests.get(url, params=params)
     response.raise_for_status()
-    return response.json()
+    answer = response.json()
+    check_response(answer)
+    return answer
 
 
 def post_to_wall(api_key, group_id, api_ver, server_answer, comment=None):
@@ -42,7 +46,9 @@ def post_to_wall(api_key, group_id, api_ver, server_answer, comment=None):
     url = f'{VK_METHODS_URL}wall.post'
     response = requests.get(url, params=params)
     response.raise_for_status()
-    return response.json()
+    answer = response.json()
+    check_response(answer)
+    return answer
 
 
 def get_uploadserver_url(api_key, group_id, api_ver):
@@ -54,7 +60,9 @@ def get_uploadserver_url(api_key, group_id, api_ver):
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
-    return response.json()['response']['upload_url']
+    answer = response.json()
+    check_response(answer)
+    return answer['response']['upload_url']
 
 
 def get_posted_comics():
@@ -69,16 +77,16 @@ def get_comic(comics_count):
     posted_comics = get_posted_comics()
     for _ in range(1, comics_count+1):
         comic_num = random.randint(1, comics_count)
-        if comic_num not in posted_comics:
+        if not posted_comics or comic_num not in posted_comics:
             break
     else:
-        raise ValueError('All comics was posted. Please, delete "posted_comics.txt" file')
+        raise ValueError('Все комиксы опубликованы. Пожалуйста, удалите файл "posted_comics.txt"')
     url = f'https://xkcd.com/{comic_num}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
     comic_content = response.json()
     return {
-        'img_url': download_comix_img(comic_content['img']),
+        'filename': download_comix_img(comic_content['img']),
         'comment': comic_content['alt'],
         'comic_num': comic_num
     }
@@ -100,17 +108,32 @@ def get_comics_count():
     return response.json()['num']
 
 
+def write_posted_comic(comic_num):
+    with open('posted_comics.txt', 'a') as posted_file:
+        posted_file.write(f'{comic_num}\n')
+
+
+def check_response(response):
+    if 'error' in response:
+        raise SystemError(
+            f'Произошла ошибка.\n'
+            f'Код ошибки: {response["error"]["error_code"]}\n'
+            f'Сообщение: {response["error"]["error_msg"]}\n'
+            f'Подробности по адресу: https://vk.com/dev/errors'
+        )
+
+
 def post_comic(api_key, group_id, api_ver):
     comic = get_comic(get_comics_count())
-    upload_url = get_uploadserver_url(api_key, group_id, api_ver)
-    answer = upload_file_to_server(upload_url, comic['img_url'])
-    answer = save_file_to_server(api_key, group_id, api_ver, answer)
-    answer = post_to_wall(api_key, group_id, api_ver, answer, comic['comment'])
-    if 'response' in answer.keys():
-        print('Комикс опубликован')
-        os.remove(comic['img_url'])
-    elif 'error' in answer.keys():
-        print('Ошибка публикации:\n', answer['error']['error_msg'])
+    try:
+        upload_url = get_uploadserver_url(api_key, group_id, api_ver)
+        answer = upload_file_to_server(upload_url, comic['filename'])
+        answer = save_file_to_server(api_key, group_id, api_ver, answer)
+        post_to_wall(api_key, group_id, api_ver, answer, comic['comment'])
+        write_posted_comic(comic['comic_num'])
+    finally:
+        os.remove(comic['filename'])
+    print('Комикс опубликован')
 
 
 if __name__ == '__main__':
